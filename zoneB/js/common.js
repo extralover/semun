97f19baa8aaeac00852405e0e7472308c6e752df
이종근 <extralover@naver.com>
2019-06-01 18:15:51 +0900

    2nd
    tax side edit & add

--- zoneB/js/common.js
+++ zoneB/js/common.js
@@ -100,4 +100,27 @@ $(document).ready(function () {
 			img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\"" + imgSrc + "\")";
 		}
 	});
+
+	// calendar
+	var dateFormat = "yy-mm-dd",
+		from = $("#from")
+			.datepicker()
+			.on("change", function () {
+				to.datepicker("option", "minDate", getDate(this));
+			}),
+		to = $("#to").datepicker()
+			.on("change", function () {
+				from.datepicker("option", "maxDate", getDate(this));
+			});
+
+	function getDate(element) {
+		var date;
+		try {
+			date = $.datepicker.parseDate(dateFormat, element.value);
+		} catch (error) {
+			date = null;
+		}
+
+		return date;
+	}
 });
