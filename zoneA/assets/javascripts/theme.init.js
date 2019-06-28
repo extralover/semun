44c629fd7278eb9ac186b3875adf952f04ce122c
이종근 <extralover@naver.com>
2019-06-29 00:21:04 +0900

    zoneA (admin site)
    zoneA (admin site) 2nd upload

--- zoneA/assets/javascripts/theme.init.js
+++ zoneA/assets/javascripts/theme.init.js
@@ -621,3 +621,168 @@
 	}
 
 }).apply(this, [ jQuery ]);
+
+// 주소 입력
+function execDaumPostcode() {
+	new daum.Postcode({
+		oncomplete: function (data) {
+			// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+			// 각 주소의 노출 규칙에 따라 주소를 조합한다.
+			// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+			var addr = ''; // 주소 변수
+			var extraAddr = ''; // 참고항목 변수
+
+			//사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+			if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+				addr = data.roadAddress;
+			} else { // 사용자가 지번 주소를 선택했을 경우(J)
+				addr = data.jibunAddress;
+			}
+
+			// 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+			if (data.userSelectedType === 'R') {
+				// 법정동명이 있을 경우 추가한다. (법정리는 제외)
+				// 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+				if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+					extraAddr += data.bname;
+				}
+				// 건물명이 있고, 공동주택일 경우 추가한다.
+				if (data.buildingName !== '' && data.apartment === 'Y') {
+					extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+				}
+				// 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+				if (extraAddr !== '') {
+					extraAddr = ' (' + extraAddr + ')';
+				}
+				// 조합된 참고항목을 해당 필드에 넣는다.
+				document.getElementById("extraAddress").value = extraAddr;
+
+			} else {
+				document.getElementById("extraAddress").value = '';
+			}
+
+			// 우편번호와 주소 정보를 해당 필드에 넣는다.
+			document.getElementById('postcode').value = data.zonecode;
+			document.getElementById("address").value = addr;
+			// 커서를 상세주소 필드로 이동한다.
+			document.getElementById("detailAddress").focus();
+		}
+	}).open();
+}
+function execDaumPostcode2() {
+	new daum.Postcode({
+		oncomplete: function (data) {
+			var addr = ''; 
+			var extraAddr = '';
+
+			if (data.userSelectedType === 'R') { 
+				addr = data.roadAddress;
+			} else {
+				addr = data.jibunAddress;
+			}
+
+			if (data.userSelectedType === 'R') {
+				if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+					extraAddr += data.bname;
+				}
+				if (data.buildingName !== '' && data.apartment === 'Y') {
+					extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+				}
+				if (extraAddr !== '') {
+					extraAddr = ' (' + extraAddr + ')';
+				}
+				document.getElementById("extraAddress2").value = extraAddr;
+
+			} else {
+				document.getElementById("extraAddress2").value = '';
+			}
+
+			document.getElementById('postcode2').value = data.zonecode;
+			document.getElementById("address2").value = addr;
+			document.getElementById("detailAddress2").focus();
+		}
+	}).open();
+}
+// 전체선택
+(function ($) {
+
+	$(".allcheck").on("click", function() {
+		var thisName = $(this).attr("name");
+		if ($(this).is(":checked")) {
+			$("input[name='" + thisName + "']").prop("checked", true);
+		} else {
+			$("input[name='" + thisName + "']").prop("checked", false);
+		}
+	});
+
+}).apply(this, [jQuery]);
+// 업무구분, 지역
+(function ($) {
+	// 업무구분
+	$(".select_ss").find("option[class^='sh']").hide();
+
+	$(".select_rr").on("change", function () {
+		$(".select_ss").find("option:first-child").prop("selected", true);
+		$(".table.chs select, .table.chs input").prop("disabled", true);
+		$(".select_ss").find("option.sh0").show();
+		if ($(this).find("option.rh1").is(":selected")) {
+			$(".select_ss").find("option[class^='sh']").hide();
+			$(".select_ss").find("option.sh1, option.sh2, option.sh3, option.sh4").show();
+		} else if ($(this).find("option.rh2").is(":selected")) {
+			$(".select_ss").find("option[class^='sh']").hide();
+			$(".select_ss").find("option.sh5, option.sh6, option.sh7, option.sh8").show();
+		} else if ($(this).find("option.rh3").is(":selected")) {
+			$(".select_ss").find("option[class^='sh']").hide();
+			$(".select_ss").find("option.sh9, option.sh10").show();
+		} else if ($(this).find("option.rh4").is(":selected")) {
+			$(".select_ss").find("option[class^='sh']").hide();
+			$(".select_ss").find("option.sh11, option.sh12, option.sh13").show();
+		}
+		
+	});
+
+	$(".table.chs select, .table.chs input").prop("disabled", true);
+	$(".select_ss").on("change", function () {
+		if ($(this).find("option.sh1").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch11, .ch12, .ch13, .ch14, .ch15, .ch16").prop("disabled", false);
+		} else if ($(this).find("option.sh2").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch11, .ch12, .ch13, .ch14, .ch15, .ch17").prop("disabled", false);
+		} else if ($(this).find("option.sh3").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch11, .ch12, .ch13, .ch14, .ch15, .ch17").prop("disabled", false);
+		} else if ($(this).find("option.sh4").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch11, .ch15, .ch17").prop("disabled", false);
+		} else if ($(this).find("option.sh5").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch21, .ch22, .ch23, .ch24, .ch25").prop("disabled", false);
+		} else if ($(this).find("option.sh6").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch31, .ch32, .ch33").prop("disabled", false);
+		} else if ($(this).find("option.sh7").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+			$(".table.chs").find(".ch41, .ch42, .ch43").prop("disabled", false);
+		} else if ($(this).find("option.sh0").is(":selected")) {
+			$(".table.chs select, .table.chs input").prop("disabled", false);
+		} else {
+			$(".table.chs select, .table.chs input").prop("disabled", true);
+		}
+	});
+
+	// 지역
+	$(".select_ar2").find("option[class^='ah']").hide();
+
+	$(".select_ar1").on("change", function () {
+		$(".select_ar2").find("option:first-child").prop("selected", true);
+		for (var i=1; i<18 ; i++) {
+			if ($(this).find("option.ah"+i).is(":selected")) {
+				$(".select_ar2").find("option[class^='ah']").hide();
+				$(".select_ar2").find("option.ah"+i).show();
+			}
+		}
+	});
+
+}).apply(this, [jQuery]);
